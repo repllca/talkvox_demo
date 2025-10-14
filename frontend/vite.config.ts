2e91218161bc778e04f914cadbc4b7b5b8703b08
Nitani Kotaro <m1m24a36@oit.ac.jp>
2025-10-14 16:22:35 +0900

    vite.config.tsファイルの編集。ローカルへの反映

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,   // ← これを追加！
+    port: 3000,   // ← 明示的に3000指定もOK
+  },
 })
